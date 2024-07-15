f1408717c4e7093fa5519cb10c15b227e30b511f
Amber Sautner <ambersautner3@gmail.com>
2024-07-15 16:19:08 -0400

    fixed icons

--- weather-app/src/weather-app.js
+++ weather-app/src/weather-app.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Weatherapp.css";
 import "./opening.css";
-import DayForecast  from "./dayforest";
+import DayForecast from "./dayforest";
 
 const Weatherapp = () => {
     const [weatherData, setWeatherData] = useState(null);
@@ -11,25 +11,20 @@ const Weatherapp = () => {
     const iconMapping = {
         "01d": '/images/sunny.gif', // example path for clear sky day
         "02d": '/images/rainy.gif', // example path for few clouds day
-        "03d": '/images/cloudy.gif', // example path for few clouds day
-            };
+        "03d": '/images/cloudy.gif', // example path for scattered clouds day
+    };
 
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchWeatherForRandomCity();
-    
-        
     }, []);
-    
 
     const fetchWeatherForRandomCity = async () => {
         const cities = ["Warren", "Bedminster", "Newark", "Trenton", "Hillsborough"];
         const randomCity = cities[Math.floor(Math.random() * cities.length)];
         fetchWeatherData(randomCity);
     };
-
-
 
     const fetchWeatherData = async (searchCity) => {
         setLoading(true);
@@ -38,34 +33,37 @@ const Weatherapp = () => {
             const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`;
             const weatherResponse = await fetch(currentWeatherUrl);
             const currentWeatherData = await weatherResponse.json();
-    
+
             // Fetch 5-day forecast
             const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`;
             const forecastResponse = await fetch(forecastUrl);
             const forecastData = await forecastResponse.json();
-    
+
             // Process the 5-day forecast data
             const processedForecastData = processForecastData(forecastData.list);
-    
+
             if (currentWeatherData.cod === 200) {
                 setWeatherData({ 
                     location: currentWeatherData.name,
-                 });
-                
+                    weatherIcon: iconMapping[currentWeatherData.weather[0].icon], // Set the weather icon here
+                    feels: currentWeatherData.main.feels_like,
+                    humidity: currentWeatherData.main.humidity,
+                    windspeed: currentWeatherData.wind.speed
+                });
             }
-    
+
             if (forecastData.cod === "200") {
                 setFiveDayForecast(processedForecastData);
             }
-            
+
             setErrorMessage("");
         } catch (error) {
-            console.error("error fetching weather data: ", error);
+            console.error("Error fetching weather data: ", error);
             setErrorMessage("Failed to fetch weather data. Please try again later.");
         }
         setLoading(false);
     };
-    
+
     // A helper function to process the 5-day forecast data
     const processForecastData = (forecastList) => {
         // Process the forecast data to get daily summaries
@@ -77,7 +75,7 @@ const Weatherapp = () => {
             }
             dailyData[day].push(forecast);
         });
-    
+
         // Reduce the data to get the high, low, and other information for each day
         return Object.keys(dailyData).map((day) => {
             const dayForecasts = dailyData[day];
@@ -87,13 +85,12 @@ const Weatherapp = () => {
                 day,
                 high: Math.max(...highs).toFixed(1),
                 low: Math.min(...lows).toFixed(1),
-                icon: dayForecasts[0].weather[0].icon,
+                iconCode: dayForecasts[0].weather[0].icon,
                 description: dayForecasts[0].weather[0].main
             };
             return daySummary;
-        }).slice(0, 5); 
+        }).slice(0, 5);
     };
-    
 
     const search = () => {
         if (city.trim() === "") {
@@ -109,77 +106,66 @@ const Weatherapp = () => {
         }
     };
 
-    
-
-
-
-
     const getBackgroundImage = (weatherCondition) => {
         switch (weatherCondition) {
             case 'Rain':
-                return 'images/rain.gif'; 
+                return 'images/rain.gif';
             case 'Clear':
                 return 'images/clouds.jpeg';
             default:
-                return 'images/clouds.jpeg'; 
+                return 'images/clouds.jpeg';
         }
     };
-    
+
     const backgroundStyle = weatherData ? { backgroundImage: `url(${getBackgroundImage(weatherData.weatherCondition)})` } : {};
 
-    
     return (
         <>
-       
-        <div className="container" style={!loading ? backgroundStyle : {}}>
-            <div className="top-bar">
-                <input
-                    type="text"
-                    className="cityInput"
-                    placeholder="Enter city name"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    onKeyPress={handleKeyPress}
-                />
-                <button className="search-icon" onClick={search}>
-                    Search
-                </button>
-            </div>
+            <div className="container" style={!loading ? backgroundStyle : {}}>
+                <div className="top-bar">
+                    <input
+                        type="text"
+                        className="cityInput"
+                        placeholder="Enter city name"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                    />
+                    <button className="search-icon" onClick={search}>
+                        Search
+                    </button>
+                </div>
 
-            {errorMessage && <div className="error-message">{errorMessage}</div>}
+                {errorMessage && <div className="error-message">{errorMessage}</div>}
 
-            {weatherData && (
-                <>
-                
-                    <div className="weather-image"> 
-                    <div className="weather-location">{weatherData.location}</div>
-                        <img src={weatherData.weatherIcon} alt="Weather icon" />
-                    </div>
-                    <div className = "weather-flexbox">
-
-                            
+                {weatherData && (
+                    <>
+                        <div className="weather-image">
+                            <div className="weather-location">{weatherData.location}</div>
+                            <img src={weatherData.weatherIcon} alt="Weather icon" />
+                        </div>
+                        <div className="weather-flexbox">
                             <div className="weather-feels">Feels Like: {weatherData.feels}</div>
                             <div className="humidity-percent">Humidity: {weatherData.humidity}</div>
                             <div className="wind-rate">Wind Speed: {weatherData.windspeed}</div>
-
-                    </div>
-                    {fiveDayForecast.length > 0 && (
-                        <div className="forecast-container">
-                            {fiveDayForecast.map((forecastData, index) => (
-                            <DayForecast
-                                key={index}
-                                day={forecastData.day} // Make sure to format the date correctly
-                                high={forecastData.high}
-                                low={forecastData.low}
-                                icon={iconMapping[forecastData.iconCode]} // Map the icon code to your gif images
-                                description={forecastData.description}
-                            />
-                            ))}
-          </div>
-        )}
-                </>
-            )}
-        </div>
+                        </div>
+                        {fiveDayForecast.length > 0 && (
+                            <div className="forecast-container">
+                                {fiveDayForecast.map((forecastData, index) => (
+                                    <DayForecast
+                                        key={index}
+                                        day={forecastData.day}
+                                        high={forecastData.high}
+                                        low={forecastData.low}
+                                        icon={iconMapping[forecastData.iconCode]}
+                                        description={forecastData.description}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </>
+                )}
+            </div>
         </>
     );
 };
